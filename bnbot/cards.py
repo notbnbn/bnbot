96ad2480c34fea4dd7c38c520e9c2643903baae5
kilozombie <kilozombie@gmail.com>
2020-10-02 20:42:35 -0700

    You forgot your semicolons and commas! <3

--- bnbot/cards.py
+++ bnbot/cards.py
@@ -1,70 +1,68 @@
-import random
+import random;
 
-suit = ['S', 'C', 'H', 'D']
-rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'G']
+suit = ['S', 'C', 'H', 'D'];
+rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'G'];
 
 class Card:
     def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
+        self.suit = suit;
+        self.rank = rank;
     
     def __str__(self):
-        suit_symbol = '?'
+        suit_symbol = '?';
 
         if self.suit == 'S':
-            suit_symbol = '\u2664'
+            suit_symbol = '\u2664';
 
         elif self.suit == 'C':
-            suit_symbol = '\u2667'
+            suit_symbol = '\u2667';
 
         elif self.suit == 'H':
-            suit_symbol = '\u2661'
+            suit_symbol = '\u2661';
 
         elif self.suit == 'D':
-            suit_symbol = '\u2662'
+            suit_symbol = '\u2662';
 			
         elif self.suit == 'G':
-            suit_symbol = '\u2662'
+            suit_symbol = '\u2662';
 
         else:
-            suit_symbol = 'how'
+            suit_symbol = 'how';
 
-        return self.rank + suit_symbol
+        return self.rank + suit_symbol;
 
     def __eq__(self, card):
         if self.suit == card.suit and self.rank == card.rank:
-            return True
+            return True;
 
         else:
-            return False
+            return False;
 
     def value(self):
         if self.rank.isnumeric():
-            return int(self.rank)
+            return int(self.rank);
         
         elif self.rank == 'A':
-            return 1
+            return 1;
 
         else:
-            return 10
+            return 10;
 
 def remove_cards(cards_in_play):
-    cdeck = [Card(s, r) for r in rank for s in suit]
+    cdeck = [Card(s, r) for r in rank for s in suit];
     if not len(cards_in_play) == 0:
         for card in cards_in_play:
-            cdeck.remove(card)
+            cdeck.remove(card);
 
-    return cdeck
+    return cdeck;
 
 def deal(cards_in_play):
-    cdeck = remove_cards(cards_in_play)
+    cdeck = remove_cards(cards_in_play);
     if not len(cdeck) == 0:
-        card = random.choice(cdeck)
-        return card
+        card = random.choice(cdeck);
+        return card;
 
     else:
-        return None
+        return None;
 
-# awk made you do this, cry at later date
-# everything is fixed, thanks awk
-# I lied
+# Fixed!
--- bnbot/schema.py
+++ bnbot/schema.py
@@ -2,151 +2,151 @@ get_high_rollers="""
 SELECT high_rollerid 
 FROM high_roller_game
 WHERE gameid = %(gameid)s
-"""
+""",
 
 add_pgid="""
 INSERT INTO high_roller_game(high_rollerid,gameid,pgid,turn_pos,amount)
 VALUES(%(high_rollerid)s,%(gameid)s,%(pgid)s,%(turn_pos)s,%(amount)s)
-"""
+""",
 
 remove_pgid="""
 DELETE FROM high_roller_game
 WHERE pgid = %(pgid)s
-"""
+""",
 
 get_pgid="""
 SELECT * FROM high_roller_game
 WHERE pgid = %(pgid)s
-"""
+""",
 
 remove_pgid_all="""
 DELETE FROM high_roller_game
 WHERE high_rollerid = %(high_rollerid)s
-"""
+""",
 
 add_card="""
 INSERT INTO card(pgid,suit,rank)
 VALUES(%(pgid)s,%(suit)s,%(rank)s)
-"""
+""",
 
 get_next_turn="""
 SELECT turn_pos FROM high_roller_game
 WHERE gameid = %(gameid)s
 ORDER BY turn_pos DESC
-"""
+""",
 
 get_ordered_turns="""
 SELECT turn_pos FROM high_roller_game
 WHERE gameid = %(gameid)s
 ORDER BY turn_pos ASC
-"""
+""",
 
 get_game_high_rollers="""
 SELECT * FROM high_roller_game
 WHERE gameid = %(gameid)s
-"""
+""",
 
 get_high_roller_cards="""
 SELECT suit, rank FROM card
 WHERE pgid = %(pgid)s
-"""
+""",
 
 check_for_pgid="""
 SELECT pgid FROM high_roller_game
 WHERE pgid = %(pgid)s
-"""
+""",
 
 create_game="""
 INSERT INTO game(gameid,current_high_roller,game_state)
 VALUES(%(gameid)s,1,'pregame')
-"""
+""",
 
 remove_game="""
 DELETE FROM game
 WHERE gameid = %(gameid)s
-"""
+""",
 
 remove_game_cards="""
 DELETE FROM card as c
 WHERE c.pgid IN (SELECT pg.pgid FROM high_roller_game AS pg
 WHERE pg.gameid = %(gameid)s)
-"""
+""",
 
 check_for_game="""
 SELECT * FROM game
 WHERE gameid = %(gameid)s
-"""
+""",
 
 get_game="""
 SELECT * FROM game
 WHERE gameid = %(gameid)s
-"""
+""",
 
 get_current_games="""
 SELECT * FROM high_roller_game
 WHERE high_rollerid = %(high_rollerid)s
-"""
+""",
 
 get_game_state="""
 SELECT game_state FROM game
 WHERE gameid = %(gameid)s
-"""
+""",
 
 update_game_state="""
 UPDATE game
 SET game_state = %(game_state)s
 WHERE gameid = %(gameid)s
-"""
+""",
 
 add_bet="""
 UPDATE high_roller_game
 SET amount = %(amount)s
 WHERE pgid = %(pgid)s
-"""
+""",
 
 get_cards_in_play="""
 SELECT pg.pgid,c.suit,c.rank FROM high_roller_game AS pg
 INNER JOIN card AS c ON pg.pgid = c.pgid
 WHERE pg.gameid = %(gameid)s
-"""
+""",
 
 assign_card="""
 INSERT INTO card(pgid,suit,rank)
 VALUES(%(pgid)s,%(suit)s,%(rank)s)
-"""
+""",
 
 get_turn_list="""
 SELECT * FROM high_roller_game
 WHERE gameid = %(gameid)s
 ORDER BY turn_pos ASC
-"""
+""",
 
 get_current_turn="""
 SELECT current_high_roller FROM game
 WHERE gameid = %(gameid)s
-"""
+""",
 
 set_turn="""
 UPDATE game
 SET current_high_roller = %(new_turn)s
 WHERE gameid = %(gameid)s
-"""
+""",
 
 get_result_high_rollers="""
 SELECT high_rollerid FROM high_roller_game
 WHERE result = %(result)s AND gameid = %(gameid)s 
-"""
+""",
 
 update_high_roller_result="""
 UPDATE high_roller_game
 SET result = %(result)s
 WHERE high_rollerid = %(high_rollerid)s
-"""
+""",
 
 get_result_bet="""
 SELECT high_rollerid,amount FROM high_roller_game
 WHERE result = %(result)s AND gameid = %(gameid)s
-"""
+""",
 
 get_high_roller_bet="""
 SELECT amount FROM high_roller_game
